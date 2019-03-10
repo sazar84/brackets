4aac328c9d0d95a258de66551fc087cd2e584c02
sazar84 <45146694+sazar84@users.noreply.github.com>
2019-03-10 03:38:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+
+// const bracketsConfig  =  [['(', ')']];
+// const str = "((()))()";
+
+function toObject(bracketsConfig) {
+  let obj = {};
+  bracketsConfig.forEach(element => obj[element[0]] = element[1]);
+  return obj;
+}
+
+var arr = [];
+arr = str.split('');
+
+
+
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  toObject(bracketsConfig);
+  if (obj.includes(arr)) {
+    return false
+  }
 }
