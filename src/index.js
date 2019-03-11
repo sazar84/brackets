9b63b5a41adc99cd1f6ce15020c5e7cdecb9fefa
sazar84 <45146694+sazar84@users.noreply.github.com>
2019-03-12 01:05:46 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// const bracketsConfig  =  [['(', ')']];
-// const str = "((()))()";
-// let Stack = [];
 var newObjOfBrackets = {};
 var arrBrackets = [];
 
@@ -22,24 +19,38 @@ module.exports = function check(str, bracketsConfig) {
   newObjOfBrackets = toObject(bracketsConfig);
   keys = Object.keys(newObjOfBrackets);
 
+  let del;
+  let delnext;
+
   for (var i = 0; i < keys.length; i++) {
-    for (var j = 0; j < arrBrackets.length; j++) {
-      if (arrBrackets.includes(keys[i])){
-        arrBrackets.splice(arrBrackets[j], 1) 
-      } else continue
+    arrBrackets.forEach((item, j) => {
+
+      if (arrBrackets.includes(keys[i])) {
+        del = arrBrackets.indexOf(keys[i]);
+      }
       if (arrBrackets.includes(newObjOfBrackets[keys[i]])) {
-        arrBrackets.splice(arrBrackets[j], 1)
-      } else continue
+        delnext = arrBrackets.indexOf(newObjOfBrackets[keys[i]]);
+      }
+      if (del < delnext) {
+        delete arrBrackets[del];
+        delete arrBrackets[delnext];
+      }
+      if (del == delnext) {
+        delete arrBrackets[del];
+        delete arrBrackets[delnext + 1];
+      }
+    });
+  }
+
+  let result;
+  for (var b = 0; b < arrBrackets.length; b++) {
+    if (typeof arrBrackets[b] == "undefined") {
+      result = true
+    } else {
+      result = false
     }
-}
+  }
 
-let result;
-if (arrBrackets.length == 0) {
-  result = true
-} else {
-  result = false
-}
-
-return result;
+  return result;
 
 }
