c88189bb7b0546d3bd2dcf772fc636496a45b69e
sazar84 <45146694+sazar84@users.noreply.github.com>
2019-03-12 22:53:42 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-var newObjOfBrackets = {};
-var arrBrackets = [];
+let newObjOfBrackets = {};
+let arrBrackets = [];
+let Stack = [];
 
 function toObject(bracketsConfig) {
   let obj = {};
@@ -13,44 +14,29 @@ function toSplit(str) {
   return arr;
 }
 
+function bracketCompare(newObjOfBrackets, Stack, bracket) {
+  if (newObjOfBrackets[bracket] === bracket) {
+    if (Stack.includes(bracket)) {
+      if (Stack.pop() !== bracket) {
+        return false;
+      }
+    } else {
+      Stack.push(bracket);
+    }
+  } else if (bracket in newObjOfBrackets) {
+    Stack.push(bracket);
+  } else if (Stack.length === 0 ||
+    newObjOfBrackets[Stack.pop()] !== bracket) {
+    return false;
+  }
+  return true;
+}
+
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  arrBrackets = toSplit(str);
-  newObjOfBrackets = toObject(bracketsConfig);
-  keys = Object.keys(newObjOfBrackets);
-
-  let del;
-  let delnext;
-
-  for (var i = 0; i < keys.length; i++) {
-    arrBrackets.forEach((item, j) => {
-
-      if (arrBrackets.includes(keys[i])) {
-        del = arrBrackets.indexOf(keys[i]);
-      }
-      if (arrBrackets.includes(newObjOfBrackets[keys[i]])) {
-        delnext = arrBrackets.indexOf(newObjOfBrackets[keys[i]]);
-      }
-      if (del < delnext) {
-        delete arrBrackets[del];
-        delete arrBrackets[delnext];
-      }
-      if (del == delnext) {
-        delete arrBrackets[del];
-        delete arrBrackets[delnext + 1];
-      }
-    });
-  }
-
-  let result;
-  for (var b = 0; b < arrBrackets.length; b++) {
-    if (typeof arrBrackets[b] == "undefined") {
-      result = true
-    } else {
-      result = false
-    }
-  }
-
-  return result;
+  let arrBrackets = toSplit(str);
+  let newObjOfBrackets = toObject(bracketsConfig);
+  
+  return Stack.length === 0;
 
 }
